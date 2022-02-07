7570cd0f0cc94adb5c6b8d9ac7381c798109b452
fmikolic <mikolicfran@gmail.com>
2022-02-07 11:27:00 +0100

    session working, user in common

--- vjezbaNette/app/Presenters/HomepagePresenter.php
+++ vjezbaNette/app/Presenters/HomepagePresenter.php
@@ -5,8 +5,50 @@ declare(strict_types=1);
 namespace App\Presenters;
 
 use Nette;
+use Nette\Application\UI\Form;
+use Nette\Security\Passwords;
 
 
 final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
+    private $passwords;
+    public function __construct(Passwords $passwords)
+    {
+        $this->passwords=$passwords;
+    }
+    protected function startup()
+    {
+        parent::startup();
+    }
+
+    function createComponentSignInForm(): Form
+    {
+        $form=new Form;
+
+        $form->addText('username', 'Username:')->setRequired('Username is empty!');
+        $form->addPassword('password', 'Password:')->setRequired('Password is empty!');
+        $form->addSubmit('submit', 'Sign in');
+
+        $form->onSuccess[]=[$this, 'signInFormSucceeded'];
+
+        return $form;
+    }
+
+    public function signInFormSucceeded(Form $form, \stdClass $data): void
+    {
+        try {
+            $this->getUser()->login($data->username, $data->password);
+            $section=$this->session->getSection("loginSection");
+            $section->set('userName', $data->username);
+            //$section->set('userName', $data->username);
+            $this->redirect('Secret:');
+        }catch (Nette\Security\AuthenticationException $e){
+            $form->addError('Login failed!');
+        }
+    }
+    public function actionOut(): void{
+        $this->getUser()->logout();
+        $this->flashMessage('Logged out');
+        $this->redirect('Homepage');
+    }
 }
--- vjezbaNette/app/Presenters/SecretPresenter.php
+++ vjezbaNette/app/Presenters/SecretPresenter.php
@@ -4,8 +4,34 @@ declare(strict_types=1);
 namespace App\Presenters;
 
 use Nette\Application\UI\Presenter;
+use Nette\Http\Session;
 
 class SecretPresenter extends Presenter
 {
+    public $session;
+    public function __construct(Session $session)
+    {
+        $this->session = $session;
+    }
+
+    protected function startup()
+    {
+
+        parent::startup();
+        $this->isLoggedUsername();
+
+    }
+
+    public function actionDefault(): void
+    {
+
+    }
+
+    public function isLoggedUsername():void {
+        $section=$this->session->getSection("loginSection");
+        $username = $section->get("userName");
+        echo $username;
+
+    }
 
 }
